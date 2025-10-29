2d04e2d9ceef9628f833e52e9a837a5ec4e9dbfa
Mochesane9 <your-email@used-for-github.com>
2025-10-29 15:16:53 +0200

    Remove check-database.js with sqlite reference

--- backend/check-database.js
+++
@@ -1,8 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('database.sqlite');
-
-const feedback = db.prepare('SELECT * FROM Feedback ORDER BY id DESC').all();
-console.log('📊 Feedback entries in database:');
-console.table(feedback);
-
-db.close();
